5af55491b1ec7d257f06ba1edfd9ea78f10894ae
Anastasiia Sereda <nastiasereda.1994@gmail.com>
2020-12-04 11:42:27 +0200

    fixed version

--- project1/src/components/page-verification/forms/imgs.jsx
+++ project1/src/components/page-verification/forms/imgs.jsx
@@ -1,3 +1,21 @@
+// import Logo from "../../logo";
+// import img from "./style.module.css";
+// const Imgs = () => {
+//   return (
+//     <>
+//       <div className={img.LogoImg}>
+//         <Logo />{" "}
+//       </div>
+//       {/* <div className={img.LogoText}></div> */}
+//       <div className={img.LeafImg}></div>
+//       <div className={img.BananaImg}></div>
+//       <div className={img.SBerryImg}></div>
+//       <div className={img.VectorImg}></div>
+//     </>
+//   );
+// };
+// export default Imgs;
+
 import img from "./style.module.css";
 const Imgs = () => {
   return (
